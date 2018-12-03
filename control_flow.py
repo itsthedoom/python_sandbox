c1dce498c591b384bb1b67343ab87e4b32fa3e46
William Doom <doomwn@gmail.com>
2018-12-02 17:26:24 -1000

    Updates
    new code

---
+++ control_flow.py
@@ -0,0 +1,83 @@
+
+#break
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+for headline in headlines:
+    news_ticker += headline + ", "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:140]
+        break
+
+print(news_ticker)
+
+
+#zip_coordinates
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+for point in zip(labels, x_coord, y_coord, z_coord):
+    points.append("{}: {}, {}, {}".format(*point))
+
+for point in points:
+    print(point)
+
+#zip_dictionary
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+
+#unzip
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+names, heights = zip(*cast)
+print(names)
+print(heights)
+
+#transpose
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+
+#enumerate
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+for i, character in enumerate(cast):
+    cast[i] = character + " " + str(heights[i])
+
+print(cast)
+
+#list_comprehension
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+#multiples_of_3
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
+
+#filter
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
--- if_statements.py
+++ if_statements.py
@@ -28,3 +28,37 @@ user = 'marie'
 
 if user not in banned_users:
     print(user.title() +", you can post a response if you wish.")
+
+
+#voting
+age = 19
+if age >=18:
+    print("You are old enough to vote!")
+    
+#amusement_park
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5")
+else:
+    print("Your admission cost is $10.")
+
+#amusement_park_2
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+print("Your admission cost is $" +str(price) + ".")
+    
+
+
+
+
+
+

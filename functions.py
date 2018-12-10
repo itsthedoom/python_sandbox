cce46df1c8ef70ee3aef3ad88d4ac7674adc470c
William Doom <doomwn@gmail.com>
2018-12-10 00:16:55 -1000

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+def which_prize(points):
+    if points <= 50:
+        return "Congrats! You have won a woden rappbit"
+    elif points <= 150:
+        return "O dear, no prize."
+    elif points <= 180:
+        return "Congrats. You have won a wafer mit"
+    else:
+        return "Congrats you won a pengui"
+    
